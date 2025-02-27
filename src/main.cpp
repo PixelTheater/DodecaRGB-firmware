c9099bf2ec5795c913768b28cc279254ae4565ed
Jeremy Seitz <7750+somebox@users.noreply.github.com>
2025-02-27 06:52:12 +0100

    cleanup and prepare main.cpp for implementation soon

--- src/main.cpp
+++ src/main.cpp
@@ -4,39 +4,7 @@
 #include <cmath>
 #include <FastLED.h>
 
-#include "points.h"
-#include "particle.h"
-#include "palettes.h"
-#include "animation_manager.h"
 
-#include <Adafruit_LSM6DSOX.h>
-
-#include "models/DodecaRGBv2/model.h"
-// or #include "models/IcosahedronV1.h"
-
-/* 
-
-# DodecaRBG V2
-- We have a dodecahedron model with 12 sides. 
-- Each side is a pentgon-shaped PCB circuit board with RGB LEDs arranged on them, spaced evenly. 
-- A Teensy 4.1 microcontroller is used to control the LEDs, using the Arduino environment and FastLED library.
-- Each pentagon side contains 104 RGB leds on a circuit board, arranged in rings from the center.
-- Each side connects to the next, in series, for a grand total of 1247 LEDs. 
-- Two hemispheres of the model are on separate channels, using pins 19 and 18 of the Teensy.
-- FastLED parallel support is being used (see https://github.com/FastLED/FastLED/releases/tag/3.9.8)
-
-## Configuration
-
-As the PCB circuit boards are wired together to form the dodecahedron, the arrangement of the sides must be 
-defined, as there are many possible configurations. In addition, the rotation of each side must be defined, 
-as it can have five possible rotations.
-
-There's a processing sketch at https://github.com/somebox/dodeca-rgb-simulator that generates the list
-of points, the X,Y,Z coordinates, and defines the order of the sides and their rotations. It also
-renders an interactive 3D model of the dodecahedron. To change your dodecahedron's configuration,
-you will need to re-generate the point mapping using this tool.
-
-*/
 
 // LED configs
 // Version is defined in VERSION file in root directory
@@ -57,48 +25,39 @@ you will need to re-generate the point mapping using this tool.
 #define BRIGHTNESS  50      // global brightness, should be used by all animations
 #define USE_IMU true        // enable orientation sensor (currently: LSM6DSOX)
 
+// model settings (replace with generated model params)
+#define NUM_LEDS 1248
+#define NUM_SIDES 12
+#define LEDS_PER_SIDE 104
+
+#define NUM_SCENES 1
+
 CRGB leds[NUM_LEDS];
-AnimationManager animation_manager(leds, NUM_LEDS, NUM_SIDES);
-Adafruit_LSM6DSOX sox;
 
 long random_seed = 0;
 int seed1,seed2 = 0;
 int mode = 0;
 
-
-
 float calculate_power_usage() {
-    float unscaled_power = calculate_unscaled_power_mW(leds, NUM_LEDS);
-    float brightness_scale = Animation::getBrightness() / 370.0f;
-    float base_power = 40.0f;  // Base power consumption in mW
-    
-    return base_power + (unscaled_power - base_power) * brightness_scale;
+  // todo
+  return 0.0f;
 }
 
 void timerStatusMessage(){
-  // print global info
+  uint8_t scene_number = 0;
+  String scene_name = "scene";
   Serial.printf("--> mode:%d (%s) @ %d FPS <--\n", 
-    mode,
-    animation_manager.getCurrentAnimationName().c_str(),
+    scene_number,
+    scene_name.c_str(),
     FastLED.getFPS() 
   );
 
-  String status = animation_manager.getCurrentAnimation()->getStatus();
+  String status = "running"; // TODO: get status from running scene
   Serial.printf("%s\n", status.c_str());
-
-// #ifdef USE_IMU
-//     // Display sensor data
-//     Serial.printf("LSM6DSOX:\n");
-//     Serial.printf("\tTemperature: %.2f deg C\n", temp.temperature);
-//     Serial.printf("\tAccel X: %.2f \tY: %.2f \tZ: %.2f m/s^2\n", 
-//       accel.acceleration.x, accel.acceleration.y, accel.acceleration.z);
-//     Serial.printf("\tGyro X: %.2f \tY: %.2f \tZ: %.2f radians/s\n", 
-//       gyro.gyro.x, gyro.gyro.y, gyro.gyro.z);
-// #endif
 
   Serial.printf("Est Power: %0.1f W (%.1f%% brightness)\n", 
     calculate_power_usage()/1000.0,
-    (Animation::getBrightness() / 255.0f) * 100);
+    (BRIGHTNESS/ 255.0f) * 100);
 }
 
 void fadeInSide(int side, int start_led, int end_led, int duration_ms) {
@@ -139,16 +98,6 @@ void setup() {
   Serial.printf("CPU Temp: %f c\n", temp);
   Serial.printf("Num LEDs: %d\n", NUM_LEDS);
   Serial.printf("Random seed: %d\n", random_seed);
-
-#ifdef USE_IMU
-  Serial.println("Adafruit LSM6DSOX check");
-  if (!sox.begin_I2C(LSM6DS_I2CADDR_DEFAULT, &Wire1)) {
-    Serial.println("LSM6DSOX not found!");
-  } else {
-    Serial.println("LSM6DSOX found");
-  }
-#endif
-
   Serial.println();
 
   pinMode(ON_BOARD_LED, OUTPUT);
@@ -181,32 +130,6 @@ void setup() {
 
   // Add animations with default settings (order defines sequence)
 
-  // animation_manager.add("identify_sides");
-  animation_manager.add("boids");
-  animation_manager.add("blobs");
-  animation_manager.add("xyz_scanner");  
-  animation_manager.add("sparkles");
-  animation_manager.add("colorshow");
-  animation_manager.add("wandering_particles");
-  animation_manager.add("geography");
-  animation_manager.add("identify_sides");
-  animation_manager.add("orientation_demo");
-
-
-  // Configure animation presets
-  animation_manager.preset("sparkles", "default");
-  animation_manager.preset("xyz_scanner", "fast");  // Try different speeds
-  animation_manager.preset("blobs", "fast");
-
-  // animation_manager.setPlaybackMode(PlaybackMode::ADVANCE, 15.0f);  // will changes animations every 15 seconds
-  // animation_manager.setPlaybackMode(PlaybackMode::RANDOM, 60.0f);  // randomly select a new animation every 60 seconds
-  animation_manager.setPlaybackMode(PlaybackMode::HOLD);  // stays on the current animation until the button is pressed
-
-  // set initial animation
-  animation_manager.setCurrentAnimation("boids");
-
-  auto model = std::make_unique<Models::DodecaRGBv2>();
-  stage.setModel(std::move(model));
 }
 
 long interval, last_interval = 0;
@@ -245,10 +168,12 @@ void loop() {
       delay(20); 
     }
     Serial.println("Button released");
-    mode = (mode + 1) % animation_manager.getPlaylistLength();
-    Serial.printf("Button pressed, changed mode to %d\n", animation_manager.getCurrentAnimationIndex());
-    animation_manager.nextAnimation();
+    mode = (mode + 1) % NUM_SCENES;
+    Serial.printf("Button pressed, changed mode to %d\n", mode);
+    // TODO: change scene
   }
-  animation_manager.update();
-  FastLED.show();
+
+  // update scene tick()
+  // update stage leds
+
 }
